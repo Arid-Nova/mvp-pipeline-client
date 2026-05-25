5da9eaf49673478b4464f73b7af21a99aa3bed53
Shakthi Weerasinghe <yasas276@gmail.com>
2026-05-25 11:21:24 -0700

    User's browser session saving and recording the end of it as well.

--- user/app/main.py
+++ user/app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, HTTPException, Depends
+from fastapi import FastAPI, HTTPException, Request, Depends
 from fastapi.middleware.cors import CORSMiddleware
 
 from .utils.verifier import verify_internal_service
@@ -6,6 +6,7 @@ from .utils.verifier import verify_internal_service
 from .services.configdb import config_db_service
 
 from .models.FeedbackRequest import FeedbackRequest
+from .models.SessionStartRequest import SessionStartRequest
 
 app = FastAPI(title="User Management Service")
 
@@ -29,3 +30,45 @@ async def save_feedback(req: FeedbackRequest):
         return {"message": "Thank you! Your feedback is recorded."}
     except():
         raise HTTPException(status_code=400)
+    
+
+@app.post("/users/session", 
+          dependencies=[Depends(verify_internal_service)], 
+          responses={
+              400: {"description": "Unacceptable Format."}
+            })
+async def start_new_user_session(req: SessionStartRequest, request: Request):
+    try:
+        # Extracting the IP address
+        ip_address = request.headers.get("X-Forwarded-For") or request.client.host
+        if ip_address and "," in ip_address:
+            ip_address = ip_address.split(",")[0].strip()
+
+        session_id = await config_db_service.save_new_session(
+            browser=req.browser,
+            screen_resolution=req.screen_resolution,
+            ip_address=ip_address
+        )
+
+        return {
+            "message": "Started a new session",
+            "session_id": session_id
+        }
+    except():
+        raise HTTPException(status_code=400)
+
+@app.post("/users/session/{session_id}/end",
+          responses={
+              400: {"description": "Invalid session ID format."},
+              404: {"description": "Session not found."}
+            })
+async def end_user_session(session_id: str):
+    was_updated = await config_db_service.end_session(session_id)
+    
+    if not was_updated:
+        raise HTTPException(
+            status_code=404, 
+            detail="Session not found or invalid session ID format."
+        )
+        
+    return {"message": "Session ended successfully"}
